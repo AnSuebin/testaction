ac0c51b317f36b789e88b3efcb01419ca5fc92cb
SooBin An <jejupalette@naver.com>
2022-10-21 20:23:09 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react';
+import { useSpeechRecognition } from 'react-speech-kit';
+
 function App() {
-  return <div className="App">h2</div>;
+  const [value, setValue] = useState('');
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      setValue(result);
+    },
+  });
+  return (
+    <div>
+      <h1>아래 버튼 클릭!</h1>
+      <button onMouseDown={listen} onMouseUp={stop}>
+        🎤
+      </button>
+      <div>{value}</div>
+      {listening && <div>음성인식 활성화중</div>}
+    </div>
+  );
 }
 
 export default App;
